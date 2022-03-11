d24a6ee5be9a35d4fca840dcb1f468796fe87e05
Lucas <lucasbsb100@gmail.com>
2022-03-11 18:03:23 -0300

    Corrige bug
    Co-authored-by: Kleidson Alves    <kleidsonalves15@gmail.com>

--- menus.pl
+++ menus.pl
@@ -2,7 +2,7 @@
 :- [utils].
 
 game_recomendation :- 
-    write('\n==========Menu de Indicação de jogos ======='),nl,
+    write('\n========== Menu de Indicação de jogos ======='),nl,
     write('[1] - Recomendação por preferencias'),nl,
     /*write('[2] - Recomendação por signo'),nl,*/
     write('[0] - Voltar ao Menu Inicial'),nl,
@@ -18,6 +18,7 @@ game_recomendation :-
     ).
 
 main_menu:-
+    clear,
     write('=========== Bem vindo ao Steam Seeker ========='),nl,
     write('[1] - Me indique um jogo'),nl,
     /*write('[2] - Adicionar um jogo a base'),nl,*/
@@ -33,7 +34,72 @@ main_menu:-
         main_menu
     ).
 
+menu_category:-
+    write('Selecione a categoria'),nl,
+    setof(X,category(X),L),
+    (display(L,0);true),
+    write('--------------------------------------'),nl,
+    write('Digite o numero da categoria'),nl,
+    read_string(user, "\n", "\r", _, Option),
+    number_string(X, Option),
+    checkinput(X,L),
+    nb_getval(checkresult,S),
+    save_preference(user_category,L, S),
+    clear.
+
+menu_genre:-
+    write('Selecione o genero'),nl,
+    setof(X,genre(X),L),
+    (display(L,0);true),
+    write('--------------------------------------'),nl,
+    write('Digite o numero do genero'),nl,
+    read_string(user, "\n", "\r", _, Option),
+    number_string(X, Option),
+    checkinput(X,L),
+    nb_getval(checkresult,S),
+    save_preference(user_genre,L, S),
+    clear.
+
+menu_publisher:-
+    write('Selecione o Editora'),nl,
+    setof(X,publisher(X),L),
+    (display(L,0);true),
+    write('0 - Nao especificar'),
+    write('--------------------------------------'),nl,
+    write('Digite o numero da editora'),nl,
+    read_string(user, "\n", "\r", _, Option),
+    number_string(X, Option),
+    checkinput(X,L),
+    nb_getval(checkresult,S),
+    save_preference(user_publisher,L, S),
+    clear.
+
+menu_developer:-
+    write('Selecione o desenvolvedor'),nl,
+    setof(X,developer(X),L),
+    (display(L,0);true),
+    write('0 - Nao especificar'),nl,
+    write('--------------------------------------'),nl,
+    write('Digite o numero referente ao desenvolvedor'),nl,
+    read_string(user, "\n", "\r", _, Option),
+    number_string(X, Option),
+    checkinput(X,L),
+    nb_getval(checkresult,S),
+    save_preference(user_developer,L, S),
+    clear.
+
+game_details(Id) :- 
+    game(Id,Name, _, _, _, _, _, _, _,Description), 
+    clear,
+    write("========== JOGO RECOMENDADO =========="),nl,
+    write("Nome:"),nl,
+    write(Name),nl,
+    write("Descrição:"),nl,
+    write(Description), nl.
+
+/*
 explore_recommandations(Id, Name) :-
+    clear,
     write('\n==============Jogo Recomendado============='), nl,
     write(Name), nl,
     write('[1] - Selecionar esse jogo'),nl,
@@ -47,25 +113,4 @@ explore_recommandations(Id, Name) :-
         ],
         explore_recommandations(Id, Name)
     ).
-
-menu_category:-
-    write('Selecione a categoria'),nl,
-    setof(X,category(X),L),
-    (display(L,0);true),
-    write('Digite o numero da categoria'),nl,
-    read_string(user, "\n", "\r", _, Option),
-    number_string(X, Option),
-    checkinput(X,L),
-    nb_getval(checkresult,S),
-    save_preference(user_category,L, S).
-
-menu_genre:-
-    write('Selecione o genero'),nl,
-    setof(X,genre(X),L),
-    (display(L,0);true),
-    write('Digite o numero do genero'),nl,
-    read_string(user, "\n", "\r", _, Option),
-    number_string(X, Option),
-    checkinput(X,L),
-    nb_getval(checkresult,S),
-    save_preference(user_genre,L, S).
+*/
